e0efe48b8cca6748b3803a0f7a8aa876583870f1
Bombaamadarona <generale0977@gmail.com>
2025-10-11 16:30:54 +0200

    Add files via upload

---
+++ shellrecentviewer.ps1
@@ -0,0 +1,52 @@
+$recentFolder = [Environment]::GetFolderPath("Recent")
+
+Write-Output "@@@@@@    @@@@@@      @@@       @@@@@@@@   @@@@@@   @@@@@@@   @@@  @@@     @@@  @@@@@@@  "
+Write-Output "@@@@@@@   @@@@@@@      @@@       @@@@@@@@  @@@@@@@@  @@@@@@@@  @@@@ @@@     @@@  @@@@@@@  "
+Write-Output "!@@       !@@          @@!       @@!       @@!  @@@  @@!  @@@  @@!@!@@@     @@!    @@!    "
+Write-Output "!@!       !@!          !@!       !@!       !@!  @!@  !@!  @!@  !@!!@!@!     !@!    !@!    "
+Write-Output "!!@@!!    !!@@!!       @!!       @!!!:!    @!@!@!@!  @!@!!@!   @!@ !!@!     !!@    @!!    "
+Write-Output " !!@!!!    !!@!!!      !!!       !!!!!:    !!!@!!!!  !!@!@!    !@!  !!!     !!!    !!!    "
+Write-Output "     !:!       !:!     !!:       !!:       !!:  !!!  !!: :!!   !!:  !!!     !!:    !!:    "
+Write-Output "    !:!       !:!       :!:      :!:       :!:  !:!  :!:  !:!  :!:  !:!     :!:    :!:    "
+Write-Output ":::: ::   :::: ::       :: ::::   :: ::::  ::   :::  ::   :::   ::   ::      ::     ::  "  
+Write-Output ":: : :    :: : :       : :: : :  : :: ::    :   : :   :   : :  ::    :      :       :"    
+Write-Output ""
+Write-Output "https://discord.gg/UET6TdxFUk"
+Write-Output "" 
+Write-Host " Elenco dei file aperti di recente tramite Shell:Recent"
+Write-Host ""
+Write-Host " Percorso: $recentFolder"
+Write-Host " Ora attuale: $(Get-Date).ToString('dd/MM/yyyy HH:mm:ss')"
+Write-Host ""
+Write-Host "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+
+Get-ChildItem -Path $recentFolder -Filter *.lnk -ErrorAction SilentlyContinue |
+    Sort-Object LastWriteTime -Descending |
+    ForEach-Object {
+        $lnkPath = $_.FullName
+        $accessTime = $_.LastAccessTime.ToString('dd/MM/yyyy HH:mm:ss')
+        $writeTime  = $_.LastWriteTime.ToString('dd/MM/yyyy HH:mm:ss')
+
+        $shell = New-Object -ComObject WScript.Shell
+        $shortcut = $shell.CreateShortcut($lnkPath)
+        $targetPath = $shortcut.TargetPath
+
+        if (![string]::IsNullOrEmpty($targetPath)) {
+            if (Test-Path $targetPath) {
+                Write-Host " File esistente:" -ForegroundColor Green
+                Write-Host " $targetPath" -ForegroundColor Yellow
+                Write-Host "     Modifica: $writeTime"
+                Write-Host "     Accesso : $accessTime"
+            } else {
+                Write-Host " File NON trovato:" -ForegroundColor Red
+                Write-Host " $targetPath" -ForegroundColor DarkRed
+                Write-Host "     Ultima modifica: $writeTime"
+                Write-Host "     Ultimo accesso: $accessTime"
+            }
+            Write-Host "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+        }
+    }
+
+Write-Host ""
+Write-Host "Operazione completata con successo." -ForegroundColor Green
+Write-Host ""
